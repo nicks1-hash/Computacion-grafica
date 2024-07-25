698a5222e5b42a06391d3912d1532abdcd78a827
nicks1-hash <est.nicolas.fernan@unimilitar.edu.co>
2024-07-25 11:03:03 -0500

    Add files via upload

---
+++ CambioCoordenadas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cambiocoordenadas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class CambioCoordenadas {
+int selecc;
+
+    public static void main(String[] args) {
+		System.out.println("\t\t====Menu====");
+		System.out.println("Seleccione la opcion que desea utilizar");
+		System.out.println("1. Convertir de coordenadas cartesianas a coordenadas polares");
+		System.out.println("2. Convertir de coordenadas polares a coordenadas cartesianas");
+	}
+	Scanner ent=new Scanner(System.in);
+	
+}
